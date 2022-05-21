dbec42bedf0d63ffe9a746b347baac27d4885e78
supakit kitjanabumrungsak <supakit.kit@thailife.com>
2022-05-22 02:17:43 +0700

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,16 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule, // firestore
     AngularFireStorageModule, // storage,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
